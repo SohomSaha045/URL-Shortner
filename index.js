09b07c1a052a1d83c92d6c15178d791a9029d751
Slutishboy <sohomsaha361@gmail.com>
2023-08-27 19:08:14 +0530

    final_

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const express=require('express');
-const router=require('./routes/url');
-const {connection}=require('./connectMongo');
-const url=require('./models/url');
-const app=express();
-const PORT=8001;
-connection('mongodb://127.0.0.1:27017/urlShortner').then(()=>{console.log('MongoDb Connected')});
+const express = require("express");
+require('dotenv').config();
+const router = require("./routes/url");
+const { connection } = require("./connectMongo");
+const url = require("./models/url");
+const app = express();
+const PORT = 8001;
+connection(process.env.db_url).then(() => {
+  console.log("MongoDb Connected");
+});
 app.use(express.json());
-app.use("/url",router);
-app.get("/:shortId",async (req,res)=>{
-    const shortId=req.params.shortId;
-    const entry=await url.findOneAndUpdate(
-    {shortId},{
-        $push:{
-            visitHistory:{
-                timestamp:Date.now()
-            }
-        }
+app.use("/url", router);
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await url.findOneAndUpdate(
+    { shortId },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
     }
-    )
-    res.redirect(entry.redirectURL);
+  );
+  res.redirect(entry.redirectURL);
+});
 
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}.....`);
 });
- 
- app.listen(PORT,()=>{
-    console.log( `Listening to port ${PORT}.....`);
-});
- 
