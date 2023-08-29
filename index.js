7e9c4dce40ee9a349997a79711f078d6c08183b4
Slutishboy <sohomsaha361@gmail.com>
2023-08-29 16:29:30 +0530

    final_commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('dotenv').config();
 const router = require("./routes/url");
+const cors = require('cors');
 const { connection } = require("./connectMongo");
 const url = require("./models/url");
 const app = express();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8000;
 connection(process.env.db_url).then(() => {
   console.log("MongoDb Connected");
 });
+app.use(cors());
 app.use(express.json());
 app.use("/url", router);
 app.get("/:shortId", async (req, res) => {
